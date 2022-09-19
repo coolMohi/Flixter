ab618fc03bf9a108bf5c38fce2e2a7bc869c85d6
coolMohi <coolmohhi@gmail.com>
2022-09-19 17:47:51 -0500

    Flix Unit 2

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Ahmed Mohammed on 9/19/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
